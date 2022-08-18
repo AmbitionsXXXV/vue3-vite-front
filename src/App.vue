4afcc3b7e5f3329957bcbf69d647f858728f9a6d
Etcetera <yilonglei790@gmail.com>
2022-08-19 01:42:29 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,19 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-slate-800 rounded-xl p-8 md:flex md:p-0">
+    <img
+      class="w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none"
+      src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg"
+    />
+    <div class="pt-6 text-center space-y-4 md:p-8 md:text-left">
+      <p class="text-lg font-medium text-zinc-100">
+        “这是使用 tailwindcss
+        来实现的一个基础的卡片区域，在实现该卡片区域的过程中，我们从来没有离开过
+        HTML 区域.”
+      </p>
+      <div class="font-medium">
+        <div class="text-sky-400">LGD_Sunday</div>
+        <div class="text-slate-500">中前台前端解决方案</div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
