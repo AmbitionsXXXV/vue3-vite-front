ae960e962f6c7269ca792f2c5f1a9fc8b83c2eee
Etcetera <yilonglei790@gmail.com>
2022-08-23 23:33:04 +0800

    新功能:系统主题变更

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,6 +1,43 @@
 import store from '@/store'
 import { watch } from 'vue'
-import { THEME_LIGHT, THEME_DARK } from '@/constants'
+import { THEME_LIGHT, THEME_DARK, THEME_SYSTEM } from '@/constants'
+
+/**
+ * 监听系统主题变更
+ */
+let matchMedia
+const watchSystemThemeChange = () => {
+    // 仅需初始化一次即可
+    if (matchMedia) return
+    matchMedia = window.matchMedia('(prefers-color-scheme: dark)')
+    // 监听主题变更
+    matchMedia.onchange = function () {
+        changeTheme(THEME_SYSTEM)
+    }
+}
+
+/**
+ * 变更主题
+ * @param {*} theme 主题的标记常量
+ */
+const changeTheme = theme => {
+    // html 的 class
+    let themeClassName = ''
+    switch (theme) {
+        case THEME_LIGHT:
+            themeClassName = 'light'
+            break
+        case THEME_DARK:
+            themeClassName = 'dark'
+            break
+        case THEME_SYSTEM:
+            watchSystemThemeChange()
+            themeClassName = matchMedia.matches ? 'dark' : 'light'
+            break
+    }
+    // 修改 html 的 class
+    document.querySelector('html').className = themeClassName
+}
 
 /**
  * 初始化主题
@@ -9,20 +46,7 @@ export default () => {
     // 1.当主题发生改变或者进入系统时，可以进行html css 的配置
     watch(
         () => store.getters.themeType,
-        (val) => {
-            // html 的 class
-            let themeClassName = ''
-            switch (val) {
-                case THEME_LIGHT:
-                    themeClassName = 'light'
-                    break
-                case THEME_DARK:
-                    themeClassName = 'dark'
-                    break
-            }
-            // 修改 html 的 class
-            document.querySelector('html').className = themeClassName
-        },
+        changeTheme,
         {
             // 初始执行一次
             immediate: true
