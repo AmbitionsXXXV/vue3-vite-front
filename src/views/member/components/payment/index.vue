6cceafad3684666c466f159eee98b8e6c64efe13
Etcetera <yilonglei790@gmail.com>
2022-08-28 23:04:36 +0800

    新功能:支付页面样式

---
+++ src/views/member/components/payment/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <!-- PC 端支付 -->
+        <pc-payment-vue v-if="!isMobileTerminal"></pc-payment-vue>
+        <!-- 移动端支付 -->
+        <mobile-payment-vue v-else></mobile-payment-vue>
+    </div>
+</template>
+
+<script setup>
+import { isMobileTerminal } from '@/utils/flexible'
+import pcPaymentVue from './pc-payment/index.vue'
+import mobilePaymentVue from './mobile-payment/index.vue'
+</script>
